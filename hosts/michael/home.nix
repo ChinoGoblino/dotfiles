cbda82f96081a7dc0cd4adfdc9a6d10e911fe436
chinoGoblino <general@ethonium.com>
2024-10-13 10:29:39 +1100

    Multi monitor support

--- hosts/michael/home.nix
+++ hosts/michael/home.nix
@@ -2,7 +2,6 @@
 
 {
 	wayland.windowManager.hyprland.settings = {
-		monitor = "DP-2, highrr, 1920x0";
-		monitor = "DP-3, highrr, 1920x200";
+		monitor = [ "DP-2, highrr, 1920x0, 1" "DP-3, 1920x1080, 0x200, 1" ];
 	};
 }
