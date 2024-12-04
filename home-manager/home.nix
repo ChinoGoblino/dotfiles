d3251bb6f06b1805c971be8f7a0e4a484c153c9e
ChinoGoblino <general@ethonium.com>
2024-12-04 15:09:43 +1100

    Added syncthing declaritively

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -76,6 +76,23 @@
 
   services.syncthing = {
 		enable = true;
+    settings = {
+      devices = {
+        "raphael" = { id = "Z2CU7Q6-PSHUE4T-LVRDYH4-KDADA43-TIGQ6QJ-LGX36Q3-EUML2MI-3GQ35AD"; };
+        "gabriel" = { id = "ABMFAT3-IPNNMZP-W2BUFHC-GSQPPBJ-AG7BPJV-YJZBGWN-6USI2YW-3VUKLAB"; };
+        "michael" = { id = "VUTC7AY-PAPVSOQ-KRZQGX6-K537K7M-FWJGI7C-TCPFZMF-WANMFPU-QDRJMAG"; };
+      };
+      folders = {
+        "Obsidian" = {
+          path = "${config.home.homeDirectory}/unsw";
+          devices = [ "gabriel" "michael" ];
+        };
+        "UNSW" = {
+          path = "${config.home.homeDirectory}/Documents/Obsidian";
+          devices = [ "gabriel" "raphael" "michael" ];
+        };
+      };
+    };
   };
 
 	xdg = {
