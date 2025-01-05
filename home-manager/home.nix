24c0a36fc5f419c006b8873603e4a55aaefe98b0
ChinoGoblino <general@ethonium.com>
2025-01-06 00:17:14 +1100

    fix: Rearranged prompt to fit k8s integration better

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -20,6 +20,7 @@
 		./hyprland.nix
     ./nvim/nvim.nix
     ./firefox.nix
+    ./starship.nix
     ./k8s/k8s.nix
   ];
     
@@ -231,48 +232,6 @@
 			'';
 		};
 
-		starship = {
-			enable = true;
-	    settings = {
-				add_newline = true;
-				username = {
-					disabled = false;
-					style_user = "#b7bdf8 bold";
-					style_root = "bright-red bold";
-					format = "[$user]($style) ";
-				};
-				line_break = {
-					disabled = true;
-				};
-				sudo = {
-					disabled = true;
-				};
-				character = {
-					success_symbol = "[❯](bold green)";
-					error_symbol = "[❯](bold red)";
-				};
-        cmd_duration = {
-          disabled = true;
-        };
-        kubernetes = {
-          disabled = false;
-          format = "[$symbol \(in $namespace \)]($style)";
-          contexts = [{
-            context_pattern = "homelab";
-            style = "#c6a0f6";
-            symbol = "  ";
-          } {
-            context_pattern = "devsoc";
-            style = "#8aadf4";
-            symbol = "  ";
-          } {
-            context_pattern = "rocketry";
-            style = "#ed8796";
-            symbol = "  ";
-          }];
-        };
-			};
-		};
 
 		git = {
 			enable = true;
