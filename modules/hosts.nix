40221accff66671fb62963ed21adc8d6b43057e4
ChinoGoblino <general@ethonium.com>
2025-01-31 00:05:45 +1100

    Removed gabriel and michael ips

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -2,13 +2,10 @@
 
 { 
   networking.extraHosts = ''
-    192.168.1.139   michael
-    192.168.1.124   gabriel
+    192.168.1.139   exodus
 
-    192.168.1.129   exodus
-
-    51.161.198.241  devnode1 
-    51.161.199.191  devnode2 
+    51.161.198.241  devnode1
+    51.161.199.191  devnode2
     51.161.162.49   rocketry
   '';
 }
